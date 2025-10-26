76e8957d824a8e3b8bdfafde6ba3e789e2c23425
maugus0 <jaiswalahan987@gmail.com>
2025-10-26 15:48:36 +0800

    Removed all CrossOrigin Annotations and disabled CORS in Security Config as it will be handled by APIGW.

--- src/main/java/com/yushan/analytics_service/controller/RankingController.java
+++ src/main/java/com/yushan/analytics_service/controller/RankingController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/ranking")
-@CrossOrigin(origins = "*")
 @Tag(name = "Ranking", description = "Ranking APIs for novels, users, and authors")
 public class RankingController {
 
