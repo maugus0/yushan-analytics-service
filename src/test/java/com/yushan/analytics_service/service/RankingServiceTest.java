1081f3c0e59f21692a90a8593b1a88b976e1f94a
maugus0 <jaiswalahan987@gmail.com>
2025-10-26 18:08:38 +0800

    Fix Unit Test Cases.

--- src/test/java/com/yushan/analytics_service/service/RankingServiceTest.java
+++ src/test/java/com/yushan/analytics_service/service/RankingServiceTest.java
@@ -1,6 +1,7 @@
 package com.yushan.analytics_service.service;
 
 import com.yushan.analytics_service.client.ContentServiceClient;
+import com.yushan.analytics_service.client.GamificationServiceClient;
 import com.yushan.analytics_service.client.UserServiceClient;
 import com.yushan.analytics_service.dto.*;
 import com.yushan.analytics_service.util.RedisUtil;
@@ -27,6 +28,9 @@ class RankingServiceTest {
     private UserServiceClient userServiceClient;
 
     @Mock
+    private GamificationServiceClient gamificationServiceClient;
+
+    @Mock
     private RedisUtil redisUtil;
 
     @InjectMocks
@@ -42,6 +46,7 @@ class RankingServiceTest {
         novelDTO.setTitle("Test Novel");
         novelDTO.setViewCnt(1000);
         novelDTO.setVoteCnt(500);
+        novelDTO.setCategoryId(1);
 
         userDTO = new UserProfileResponseDTO();
         userDTO.setUuid(UUID.randomUUID().toString());
@@ -50,38 +55,63 @@ class RankingServiceTest {
 
     @Test
     void testRankNovel_Success() {
-        Set<String> novelIds = new LinkedHashSet<>(Arrays.asList("1"));
-        when(redisUtil.zCard(anyString())).thenReturn(1L);
-        when(redisUtil.zReverseRange(anyString(), anyLong(), anyLong())).thenReturn(novelIds);
-
-        ApiResponse<List<NovelDetailResponseDTO>> response = new ApiResponse<>();
+        // Mock the content service response
+        PageResponseDTO<NovelDetailResponseDTO> pageData = new PageResponseDTO<>();
+        pageData.setContent(Arrays.asList(novelDTO));
+        pageData.setTotalElements(1);
+        pageData.setHasNext(false);
+        
+        ApiResponse<PageResponseDTO<NovelDetailResponseDTO>> response = new ApiResponse<>();
         response.setCode(200);
-        response.setData(Arrays.asList(novelDTO));
-        when(contentServiceClient.getNovelsBatch(anyList())).thenReturn(response);
+        response.setData(pageData);
+        
+        when(contentServiceClient.getNovels(anyInt(), anyInt(), anyString(), anyString()))
+                .thenReturn(response);
 
         PageResponseDTO<NovelDetailResponseDTO> result = 
                 rankingService.rankNovel(0, 20, "view", null, null);
 
         assertNotNull(result);
-        verify(redisUtil, times(1)).zCard(anyString());
+        assertNotNull(result.getContent());
+        assertEquals(1, result.getTotalElements());
+        verify(contentServiceClient, atLeastOnce()).getNovels(anyInt(), anyInt(), anyString(), anyString());
     }
 
     @Test
     void testRankUser_Success() {
-        Set<String> userUuids = new LinkedHashSet<>(Arrays.asList(userDTO.getUuid()));
-        when(redisUtil.zCard(anyString())).thenReturn(1L);
-        when(redisUtil.zReverseRange(anyString(), anyLong(), anyLong())).thenReturn(userUuids);
-
-        ApiResponse<List<UserProfileResponseDTO>> response = new ApiResponse<>();
-        response.setCode(200);
-        response.setData(Arrays.asList(userDTO));
-        when(userServiceClient.getUsersBatch(anyList())).thenReturn(response);
+        // Mock user service response
+        PageResponseDTO<UserProfileResponseDTO> pageData = new PageResponseDTO<>();
+        pageData.setContent(Arrays.asList(userDTO));
+        pageData.setTotalElements(1);
+        pageData.setHasNext(false);
+        
+        ApiResponse<PageResponseDTO<UserProfileResponseDTO>> userResponse = new ApiResponse<>();
+        userResponse.setCode(200);
+        userResponse.setData(pageData);
+        
+        when(userServiceClient.getAllUsersForRanking(anyInt(), anyInt(), anyString(), anyString()))
+                .thenReturn(userResponse);
+        
+        // Mock gamification service response
+        GamificationServiceClient.GamificationStats stats = new GamificationServiceClient.GamificationStats();
+        stats.userId = userDTO.getUuid();
+        stats.level = 5;
+        stats.currentExp = 1000;
+        
+        ApiResponse<List<GamificationServiceClient.GamificationStats>> gamificationResponse = new ApiResponse<>();
+        gamificationResponse.setCode(200);
+        gamificationResponse.setData(Arrays.asList(stats));
+        
+        when(gamificationServiceClient.getBatchUsersStats(anyList()))
+                .thenReturn(gamificationResponse);
 
         PageResponseDTO<UserProfileResponseDTO> result = 
                 rankingService.rankUser(0, 20, "overall");
 
         assertNotNull(result);
-        verify(redisUtil, times(1)).zCard(anyString());
+        assertNotNull(result.getContent());
+        verify(userServiceClient, atLeastOnce()).getAllUsersForRanking(anyInt(), anyInt(), anyString(), anyString());
+        verify(gamificationServiceClient, atLeastOnce()).getBatchUsersStats(anyList());
     }
 
     @Test
